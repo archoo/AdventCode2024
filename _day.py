9873eb627daade688b4174bc0133f8c11af44cbe
Daniel Hoolihan <daniel@hoolihan.org>
2024-10-19 20:25:28 +1000

    new year basic setup

--- _day.py
+++ _day.py
@@ -3,9 +3,19 @@ from pathlib import Path
 
 parser = argparse.ArgumentParser(prog='AoC DayFile Creator')
 parser.add_argument('day_num',type=int,help='day of the advent to create files for')
+parser.add_argument('--nocode',action='store_true',help='add basic code to "read day_input.txt as array of lines')
 arg = parser.parse_args()
 
 d = str(arg.day_num).rjust(2,'0')
 Path(f'day_{d}.txt').touch()
-Path(f'day_{d}.py').touch()
 Path(f'day_{d}_test.txt').touch()
+
+if arg.nocode:
+  Path(f'day_{d}.py').touch()
+else:
+  with open(f'day_{d}.py','wt',newline='\n') as sf:
+    sf.write('import sys,os,re,itertools\n\n')
+    sf.write(f"rawdata = [r.strip() for r in open('day_{d}_test.txt','rt').readlines()]\n")
+    sf.write("for row in rawdata:\n  print(row)\n")
+  
+    
